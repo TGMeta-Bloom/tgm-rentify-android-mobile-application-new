5ef0b8c1d1fd11da0f43da3745fe424c786d682e
Thamasha3 <nethuthamasha3@gmail.com>
2025-12-15 02:04:38 +0530

    Feat: Implement Profile UI and navigation from BottomNav

---
+++ app/src/main/java/com/example/myapplication/view/LandlordAddPropertyFragment.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class LandlordAddPropertyFragment : Fragment(R.layout.fragment_landlord_add_property) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Placeholder
+    }
+}
